b7124f4034d08d5b70a8b4fe20db1767af5fa3e1
Niyush-04 <mauryapradip04@gmail.com>
2025-07-11 13:42:47 +0530

    carousel

--- js/script.js
+++ js/script.js
@@ -33,3 +33,25 @@ document.querySelectorAll(".feature-card").forEach((card) => {
   card.style.transition = "opacity 0.6s ease, transform 0.6s ease";
   observer.observe(card);
 });
+
+//carousel
+const swiper = new Swiper(".mySwiper", {
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 30,
+  centeredSlides: true,
+  grabCursor: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: { slidesPerView: 1 },
+    768: { slidesPerView: 2 },
+    1024: { slidesPerView: 3 },
+  },
+});
